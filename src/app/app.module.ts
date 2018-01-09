076944bf050ca29011ac3c33bdcc0c7bd02dc337
Tlahey <a.vendeville@orange.fr>
2018-01-09 18:56:07 +0100

    add flex

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-
+import { FlexLayoutModule } from "@angular/flex-layout";
 
 import { AppComponent } from './app.component';
 import {
@@ -71,6 +71,7 @@ import {
     MatTabsModule,
     MatToolbarModule,
     MatTooltipModule,
+    FlexLayoutModule
   ]
 })
 export class MaterialModule {}
