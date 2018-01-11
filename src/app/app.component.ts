0ad5ea976d7a65bb140ee4793ad3f19f021025e3
Antoine Vendeville <Antoine@DESKTOP-3UKG8IN.cimail-info.fr>
2018-01-11 22:33:24 +0100

    Ajout des informations pannel de droite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('fadeInOut', [
+      transition(':enter', [   // :enter is alias to 'void => *'
+        style({ opacity: 0 }),
+        animate(500, style({ opacity: 1 }))
+      ])
+    ])
+  ]
 })
 export class AppComponent {
-
+  public random(){
+    return Math.floor(Math.random() * 100) + 1;
+  }
 }
